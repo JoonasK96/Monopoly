43018e4e702a8864c0b28f4eb85cf02382ce1ffa
BuildTools <ollipallo98@gmail.com>
2021-12-13 14:54:56 +0200

    post destruction, fixed, still not working

--- app/src/main/java/com/example/projektitest/ScoreViewModel.kt
+++ app/src/main/java/com/example/projektitest/ScoreViewModel.kt
@@ -1,7 +1,21 @@
 package com.example.projektitest
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ScoreViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val players: MutableLiveData<List<Player>> by lazy {
+        MutableLiveData<List<Player>>().also {
+            loadUsers()
+        }
+    }
+
+    fun getUsers(): LiveData<List<Player>> {
+        return players
+    }
+
+    private fun loadUsers() {
+        // Do an asynchronous operation to fetch users.
+    }
 }
