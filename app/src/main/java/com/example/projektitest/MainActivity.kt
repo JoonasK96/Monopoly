d91e2a2f8adc27ac4a6f75659c7c7aff73f6d816
BuildTools <ollipallo98@gmail.com>
2021-12-14 13:55:01 +0200

    ALMOST DONE

--- app/src/main/java/com/example/projektitest/MainActivity.kt
+++ app/src/main/java/com/example/projektitest/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.projektitest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     var num = 0
     lateinit var binding: ActivityMainBinding
+    var playerN = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,49 +26,66 @@ class MainActivity : AppCompatActivity() {
         val add1 = findViewById<Button>(R.id.buttonAdd1)
         val add2 = findViewById<Button>(R.id.buttonAdd2)
         val add3 = findViewById<Button>(R.id.buttonAdd3)
+        val b1 = findViewById<Button>(R.id.button1)
+        val b2 = findViewById<Button>(R.id.button2)
+        val b3 = findViewById<Button>(R.id.button3)
+        val b4 = findViewById<Button>(R.id.button4)
 
         val textView = findViewById<TextView>(R.id.textView)
 
 
-        dec1.setOnClickListener{
-         num--
-         textView.text = num.toString()
-         }
-        dec2.setOnClickListener{
+        dec1.setOnClickListener {
+            num--
+            textView.text = num.toString()
+        }
+        dec2.setOnClickListener {
             num -= 10
             textView.text = num.toString()
         }
-        dec3.setOnClickListener{
+        dec3.setOnClickListener {
             num -= 100
             textView.text = num.toString()
         }
-        add1.setOnClickListener{
+        add1.setOnClickListener {
             num++
             textView.text = num.toString()
         }
-        add2.setOnClickListener{
+        add2.setOnClickListener {
             num += 10
             textView.text = num.toString()
         }
-        add3.setOnClickListener{
+        add3.setOnClickListener {
             num += 100
             textView.text = num.toString()
         }
-
+        b1.setOnClickListener {
+            playerN = 0
+        }
+        b2.setOnClickListener {
+            playerN = 1
+        }
+        b3.setOnClickListener {
+            playerN = 2
+        }
+        b4.setOnClickListener {
+            playerN = 3
+        }
         binding.buttonNext.setOnClickListener {
-
+            var players =
+            Player.updateMoney(players, playerN, 200)
+            Log.d("PlayerList", players.toString())
         }
 
     }
 
-   /* val firstFragment = FirstFragment()
-    supportFragmentManager.beginTransaction().replace() */
+    /* val firstFragment = FirstFragment()
+     supportFragmentManager.beginTransaction().replace() */
 
     private fun replaceFragment(fragment: Fragment) {
 
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragmentContainer,fragment)
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
         fragmentTransaction.commit()
     }
 
--- app/src/main/java/com/example/projektitest/Player.kt
+++ app/src/main/java/com/example/projektitest/Player.kt
@@ -1,22 +1,20 @@
 package com.example.projektitest
 
-class Player(val name: String, val money: Int){
+class Player(val name: String, var money: Int) {
     companion object {
         private var lastPlayerId = 0
-        fun createPlayersList(numPlayers: Int) : ArrayList<Player> {
+        fun createPlayersList(numPlayers: Int): ArrayList<Player> {
             val players = ArrayList<Player>()
             for (i in 1..numPlayers) {
-                players.add(Player("Player " + ++lastPlayerId, 1000))
-            }
-            return players
-        }
-        fun updateMoney(numPlayers: Int) : ArrayList<Player>{
-            val players = ArrayList<Player>()
-            for (i in 1..numPlayers) {
-                players.add(Player("Player" + ++lastPlayerId, 1000))
+                players.add(Player("Player " + ++lastPlayerId, 0))
             }
             return players
         }
 
-}
+        fun updateMoney(players:ArrayList<Player>, playerN: Int, Money: Int): ArrayList<Player> {
+            players[playerN].money = Money
+            return players
+        }
+
+    }
 }
